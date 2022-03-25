051d2fc8ec5c337ee3e948c3acf2e1e0b78521b2
TANUJ RAJKUMAR <medicustanuj@gmail.com>
2022-03-25 20:32:21 +0530

    FILE DOWNLOAD FROM cPanel

--- admin/database.php
+++ admin/database.php
@@ -1,9 +1,7 @@
 <?php
 $dbHost = 'localhost';
-$dbUser = 'root';
-$dbPass = '';
+$dbUser = 'INC_usr';
+$dbPass = 'zE-MxE2i(Bzk';
 $dbName = 'cms_inc';
 $dbConn = mysqli_connect ($dbHost, $dbUser, $dbPass) or die ('MySQL connect failed. ' . mysqli_error($dbConn));
 mysqli_select_db($dbConn,$dbName) or die('Cannot select database. ' . mysqli_error($dbConn));
-
-// $secret = "6Lefda8dAAAAAHgwTBamNP7qiw_T4rkJLhQ-GMQp";
